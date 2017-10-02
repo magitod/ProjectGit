d02af29d1386191f85422164a72d8dfc0f49d4ab
Alex Psesovskih <plesovskih@mintmail.ru>
2017-10-02 16:30:09 +0500

    #Синхро

---
+++ ProjectGit/Layer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGit
+{
+    class Layer : ILayer
+    {
+        int inputDimension_;
+        double[] lastOutput_;
+        INeuron[] neurons_;
+
+        public int InputDimension { get { return inputDimension_; } }
+
+        public double[] LastOutput { get { return lastOutput_; } }
+
+        public INeuron[] Neurons { get { return neurons_; } }
+
+        public double[] compute(double[] inputVector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
